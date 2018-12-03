6b15f44870fc520367b937a83270896bb8c34910
busoff <busoff@qq.com>
2018-12-03 23:14:42 +0800

    remove the prefix report for the existing urls

--- hello/urls.py
+++ hello/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = 'projecttime'
 urlpatterns = [
-    path('report/entries/', views.get_entries, name='get_entries'),
-    path('report/submit_entries', views.submit_entries, name='submit_entries'),
-    path('report/projects', views.get_projects, name="report"),
-    path('report/<int:user_id>', views.projecttime, name="report"),
+    path('projects', views.get_projects, name="get_projects"),
+    path('entries', views.get_entries, name='get_entries'),
+    path('submit_entries', views.submit_entries, name='submit_entries'),
+    path('<int:user_id>', views.projecttime, name="user"),
     path('', views.index, name="index"),
     path('test', views.test, name="test"),
 ]
